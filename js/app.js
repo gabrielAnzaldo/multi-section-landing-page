3ffe9ffa2d165aa290052566ec26c5c544be75c1
Gabriel Anzaldo <gabriel@sellry.com>
2020-10-12 19:56:50 -0300

    chore: build basic, navigation.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 import '../css/styles.css';
+// get all sections
+const sections = document.querySelectorAll('section');
+// get ul
+const navBar = document.querySelector('#navbar__list');
+// build nav
+const fragment = document.createDocumentFragment();
+for(let index =0; index < sections.length; index++) {
+  const navItem = document.createElement('li');
+  const currentSectionTitle = sections[index].dataset.nav;
+  navItem.innerText = currentSectionTitle;
+  fragment.appendChild(navItem);
+}
+navBar.appendChild(fragment);
